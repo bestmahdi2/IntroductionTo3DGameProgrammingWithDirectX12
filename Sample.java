bda478ae73fd46ab3b3400428d22f6f0af00d45d
Master <alibad979@gmail.com>
2023-07-20 20:41:12 +0330

    initial commit

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample {
+    int fibonacci(int n) {
+        if (n < 2)
+            return 1;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
---
+++ sample.py
@@ -0,0 +1,4 @@
+def fibonacci(n):
+    if n < 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
